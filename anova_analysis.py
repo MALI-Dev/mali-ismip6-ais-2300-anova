b7ad344a70ed9f5d5ad236c9fe42f473cbee5826
Matthew Hoffman <mhoffman@lanl.gov>
2025-01-22 10:30:45 -0800

    update anova model to include q
    for now the interaction terms are commented out
    (and the 3-way interaction terms are not added at all)

--- anova_analysis.py
+++ anova_analysis.py
@@ -32,6 +32,7 @@ slr_list = np.zeros((n_files, n_years)) * np.nan
 
 for i, file_path in enumerate(file_list):
     run_name = os.path.basename(file_path).split('.')[0]
+    print(run_name)
     q_list[i] = run_name.split('_')[0].split('-')[1]
     m_list[i] = run_name.split('_')[1].split('-')[1]
     e_list[i] = run_name.split('_')[2].split('-')[1]
@@ -49,10 +50,13 @@ var_q = np.zeros((n_years,)) * np.nan
 var_m = np.zeros((n_years,)) * np.nan
 var_e = np.zeros((n_years,)) * np.nan
 var_h = np.zeros((n_years,)) * np.nan
+var_qm = np.zeros((n_years,)) * np.nan
+var_qe = np.zeros((n_years,)) * np.nan
+var_qh = np.zeros((n_years,)) * np.nan
 var_me = np.zeros((n_years,)) * np.nan
 var_mh = np.zeros((n_years,)) * np.nan
 var_eh = np.zeros((n_years,)) * np.nan
-var_meh = np.zeros((n_years,)) * np.nan
+var_qmeh = np.zeros((n_years,)) * np.nan
 var_res = np.zeros((n_years,)) * np.nan
 for yr_idx, yr in enumerate(year_list):
     print(f'\n***** Year={yr} (ind={yr_idx}) *****\n')
@@ -71,28 +75,48 @@ for yr_idx, yr in enumerate(year_list):
                        'e': [e_list[i] for i in valid_run_ind],
                        'h': [h_list[i] for i in valid_run_ind],
                        'slr': slr_vals})
-    i#print(df)
+    print(df)
     #model = ols("""slr ~ C(q) + C(m) + C(e) + C(h) +
     #           C(q):C(m) + C(q):C(e) + C(q):C(h) +
-    #           C(m):C(e) + C(m):C(h) + C(e):C(h) +
+    #           C(m):C(e) + C(m):C(h) +
+    #           C(e):C(h) +
     #           C(q):C(m):C(e):C(h)""", data=df).fit()
-    model = ols("""slr ~ C(m) + C(e) + C(h) +
-               C(m):C(e) + C(m):C(h) + C(e):C(h) +
-               C(m):C(e):C(h)""", data=df).fit()
+    model = ols("""slr ~ C(q) + C(m) + C(e) + C(h)""", data=df).fit()
+    #model = ols("""slr ~ C(m) + C(e) + C(h) +
+    #           C(m):C(e) + C(m):C(h) + C(e):C(h) +
+    #           C(m):C(e):C(h)""", data=df).fit()
 
+    print(model)
 
     anova_out = sm.stats.anova_lm(model, typ=2)
     print(anova_out)
-    var_m[yr_idx] = anova_out.sum_sq[0] / n_valid
-    var_e[yr_idx] = anova_out.sum_sq[1] / n_valid
-    var_h[yr_idx] = anova_out.sum_sq[2] / n_valid
-    var_me[yr_idx] = anova_out.sum_sq[3] / n_valid
-    var_mh[yr_idx] = anova_out.sum_sq[4] / n_valid
-    var_eh[yr_idx] = anova_out.sum_sq[5] / n_valid
-    var_meh[yr_idx] = anova_out.sum_sq[6] / n_valid
-    var_res[yr_idx] = anova_out.sum_sq[7] / n_valid
+    idx = 0
+    var_q[yr_idx] = anova_out.sum_sq[idx] / n_valid
+    idx += 1
+    var_m[yr_idx] = anova_out.sum_sq[idx] / n_valid
+    idx += 1
+    var_e[yr_idx] = anova_out.sum_sq[idx] / n_valid
+    idx += 1
+    var_h[yr_idx] = anova_out.sum_sq[idx] / n_valid
+    idx += 1
+#    var_qm[yr_idx] = anova_out.sum_sq[idx] / n_valid
+#    idx += 1
+#    var_qe[yr_idx] = anova_out.sum_sq[idx] / n_valid
+#    idx += 1
+#    var_qh[yr_idx] = anova_out.sum_sq[idx] / n_valid
+#    idx += 1
+#    var_me[yr_idx] = anova_out.sum_sq[idx] / n_valid
+#    idx += 1
+#    var_mh[yr_idx] = anova_out.sum_sq[idx] / n_valid
+#    idx += 1
+#    var_eh[yr_idx] = anova_out.sum_sq[idx] / n_valid
+#    idx += 1
+#    var_qmeh[yr_idx] = anova_out.sum_sq[idx] / n_valid
+#    idx += 1
+    var_res[yr_idx] = anova_out.sum_sq[idx] / n_valid
 
-var_all = var_m + var_e + var_h + var_me + var_mh + var_eh + var_meh + var_res
+#var_all = var_m + var_e + var_h + var_me + var_mh + var_eh + var_qmeh + var_res
+var_all = var_m + var_e + var_h + var_res
 
 # plot
 
@@ -104,22 +128,34 @@ ax1 = fig.add_subplot(nrow, ncol, 1)
 thk=2
 thn=0.5
 ax1.plot(year_list, np.sqrt(var_all), label='all', color='k', linewidth=thk)
+ax1.plot(year_list, np.sqrt(var_q), label='q', linewidth=thk)
 ax1.plot(year_list, np.sqrt(var_m), label='m', linewidth=thk)
 ax1.plot(year_list, np.sqrt(var_e), label='e', linewidth=thk)
 ax1.plot(year_list, np.sqrt(var_h), label='h', linewidth=thk)
-ax1.plot(year_list, np.sqrt(var_me), label='me', linewidth=thn)
-ax1.plot(year_list, np.sqrt(var_mh), label='mh', linewidth=thn)
-ax1.plot(year_list, np.sqrt(var_eh), label='eh', linewidth=thn)
-ax1.plot(year_list, np.sqrt(var_meh), label='meh', linewidth=thn)
+#ax1.plot(year_list, np.sqrt(var_qm), label='qm', linewidth=thn)
+#ax1.plot(year_list, np.sqrt(var_qe), label='qe', linewidth=thn)
+#ax1.plot(year_list, np.sqrt(var_qh), label='qh', linewidth=thn)
+#ax1.plot(year_list, np.sqrt(var_me), label='me', linewidth=thn)
+#ax1.plot(year_list, np.sqrt(var_mh), label='mh', linewidth=thn)
+#ax1.plot(year_list, np.sqrt(var_eh), label='eh', linewidth=thn)
+#ax1.plot(year_list, np.sqrt(var_qmeh), label='qmeh', linewidth=thn)
 ax1.plot(year_list, np.sqrt(var_res), label='residual', linewidth=thn)
 plt.legend()
 plt.xlabel('Year')
 plt.ylabel('sigma (m SLE)')
 
 ax2 = fig.add_subplot(nrow, ncol, 2)
-stackdata = np.stack([var_m, var_e, var_h, var_me, var_mh, var_eh, var_meh, var_res])
+stackdata = np.stack([var_q, var_m, var_e, var_h,
+                      #var_qm, var_qe, var_qh,
+                      #var_me, var_mh, var_eh,
+                      #var_qmeh,
+                      var_res])
 stackdata = stackdata / stackdata.sum(axis=0) * 100.0
-ax2.stackplot(year_list, stackdata, labels=('m', 'e', 'h', 'me', 'mh', 'eh', 'meh', 'residual'))
+ax2.stackplot(year_list, stackdata, labels=('q', 'm', 'e', 'h',
+                                            #'qm', 'qe', 'qh',
+                                            #'me', 'mh', 'eh',
+                                            #'qmeh',
+                                            'residual'))
 plt.legend()
 plt.xlabel('Year')
 plt.ylabel('percentage of variance')
